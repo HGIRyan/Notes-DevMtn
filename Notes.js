569d4e0177b84f5cd69b35cf5c73bea909baf335
Ryan Hutchison <ryanzhutch@gmail.com>
2018-11-06 10:50:51 -0700

    General first week notes

---
+++ Notes.js
@@ -0,0 +1,70 @@
+/* 
+
+
+Terminal
+
+    -touch (fileName.ext) ---- to create new file
+    -rm is to remove/delete
+    -rm -rf will delete folder
+
+
+Git
+    -git add <filename>
+    -git commit -m 'Message'
+    -git init, git status, red it not in staging, green is
+    -git add . ---- all files go into staging area
+    -if you get stuck in vin do :q
+
+
+Github
+    -git hub is an online resource to save your code
+    Flow up
+    -Githup
+    -git
+    -computer
+
+    -
+
+
+
+
+Functions
+    -
+
+Arrays
+    -for(var i =0; i<3; i++) 
+        -first -- where in array loop starts
+        -second -- checks fo logic
+        -third -- runs logic {}
+        -fourth -- increments the value of i to loop again until value of i does not meet requirements of step 2
+    -.push adds to the end of the array
+    -.unshift adds to the beginning of the array
+    -.pop removes from the end of the array
+    -.shift removes from the beginning of the array
+    -.splice is used to remove items from array (where to start in array,how many items to remove) used to add items in (where to start,0 items to remove,'element to insert')
+    -.slice used to get copies of an entire array or just peices of it (start at index, what index to stop at) does not modify the array, coppies to get elements out
+    -
+Objects
+    -Example for how to change a object within a function
+            -var user = {
+                name: 'Sally Rally',
+                pwHash: 'U+Ldlngx2BYQk',
+                username: 'SallyRally801'
+            };
+
+            function personalize(obj) {
+            for(var key in user){
+                user.name='Ryan',
+                user.pwHash='43df90w_h',
+                user.username='ryanleeeallred';
+            }
+            return user;
+            }
+
+callback
+    -setTimeout(function,time in milliseconds delayed) 
+    -
+
+
+
+*/
