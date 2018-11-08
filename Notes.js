589c3f7d97baf02448c60ee3093ded4c2e69ad35
Ryan Hutchison <ryanzhutch@gmail.com>
2018-11-08 08:27:49 -0700

    Notes

--- Notes.js
+++ Notes.js
@@ -44,6 +44,34 @@ Arrays
     -.splice is used to remove items from array (where to start in array,how many items to remove) used to add items in (where to start,0 items to remove,'element to insert')
     -.slice used to get copies of an entire array or just peices of it (start at index, what index to stop at) does not modify the array, coppies to get elements out
     -
+    -How to make a string numbers only using parseInt()
+                            var beeSting = [1,'2',4]
+                            function notAString(arr){
+                                newArr = [];
+                                for( var i = 0; i < arr.length; i++){
+                                newArr.push(parseInt(arr[i]))
+                                }
+                               return newArr;
+                            }
+                            notAString(beeSting)
+                          
+    -.indexOf() -- if the search is not in array indexOf will return -1, if search is in array itll return the index of the searched Item
+    -.forEach -- go through each item 1 at a time and pass it through a function we give it. -- does not return a value
+    -.map works just like a .forEach but returns a value in a new empty array
+    -.filter -- filters array through criteria -- 
+                                var names = ['suzie', 'Ben', 'mark', 'frankline]
+                                var shortNames = names.filter(function(val,i,arr){
+                                    return val.length < 5;
+                                })
+                                shortNames = ['ben', 'mark']
+                                names = ['suzie', 'Ben', 'mark', 'frankline]
+    -
+
+
+
+
+
+
 Objects
     -Example for how to change a object within a function
             -var user = {
@@ -60,10 +88,43 @@ Objects
             }
             return user;
             }
+    -Delete -- delete is a keyword -- delete myObj.property
+                var backpack = {
+                    oldLaptop: 'slow',
+                    oldLunch: 'moldy',
+                    pencil:'sharp,
+                }
+                var oldStuff = ['oldLaptop', 'oldLunch']
+                for (var i = 0; i<oldStuff.length; i++){
+                    delete backpack [oldStuff[i];]
+                }
+
+    -copying with an assign
+                -newObj = Object.assign({},obj) (capital 'O' in Object.assign)
+    - for in loops
+                for(var variable in obj){
+                    console.log(obj[variable])
+                }
+    
 
 callback
     -setTimeout(function,time in milliseconds delayed) 
     -
+
+
+
+
+
+
+    to-do
+        -objects - For In Loops and the rest of preclass reading
+        -3.1 Arrays Practice Problem
+        -3.2 Arrays Practice Problem
+        -3.3 Arrays Practice Problem
+        -3.4 Arrays Practice Problem
+        
+
+
 
 
 
